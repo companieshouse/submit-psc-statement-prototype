f97f294d99e563bef43984a10fcbecd000596c7e
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2025-03-12 13:20:30 +0000

    v10 added

--- app/routes.js
+++ app/routes.js
@@ -17,6 +17,7 @@ router.use('/', require('./routes/v6-routes.js'))
 router.use('/', require('./routes/v7-routes.js'))
 router.use('/', require('./routes/v8-routes.js'))
 router.use('/', require('./routes/v9-routes.js'))
+router.use('/', require('./routes/v10-routes.js'))
 
 // DEEP
 
