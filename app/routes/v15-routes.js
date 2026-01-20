1ca1db4866ab80da0c7802fed0a3ebab2671b7ca
phoeberadford <pradford@companieshouse.gov.uk>
2026-01-20 12:08:42 +0000

    v15 routing updates so far

--- app/routes/v15-routes.js
+++ app/routes/v15-routes.js
@@ -18,6 +18,31 @@ router.use((req, res, next) => {
 
 // first journey!
 
+router.post('/v15/first-extension/start-v2', function(request, response) {
+    response.redirect('/v15/first-extension/create-or-sign-in')
+})
+
+router.post('/v15/first-extension/create-or-sign-in', function(request, response) {
+    response.redirect('/v15/first-extension/sign-in-email')
+})
+
+router.post('/v15/first-extension/sign-in-email', function(request, response) {
+    response.redirect('/v15/first-extension/sign-in-password')
+})
+
+router.post('/v15/first-extension/sign-in-password', function(request, response) {
+    response.redirect('/v15/first-extension/company-number')
+})
+
+router.post('/v15/first-extension/company-number', function(request, response) {
+    response.redirect('/v15/first-extension/confirm-company')
+})
+
+router.post('/v15/first-extension/confirm-company', function(request, response) {
+    response.redirect('/v15/first-extension/individual/psc-list')
+})
+
+
 
 
 
